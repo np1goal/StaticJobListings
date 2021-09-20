4022f870df7f78fafd778a2c90e31523af06914b
np1goal <np1goal@gmail.com>
2021-09-20 00:35:13 -0700

    First commit
    The style is finished. //TODO: Filter not working well

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,205 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  datas = [
+    {
+      "id": 1,
+      "company": "Photosnap",
+      "logo": "../assets/photosnap.svg",
+      "new": true,
+      "featured": true,
+      "position": "Senior Frontend Developer",
+      "role": "Frontend",
+      "level": "Senior",
+      "postedAt": "1d ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["HTML", "CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 2,
+      "company": "Manage",
+      "logo": "../assets/manage.svg",
+      "new": true,
+      "featured": true,
+      "position": "Fullstack Developer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1d ago",
+      "contract": "Part Time",
+      "location": "Remote",
+      "languages": ["Python"],
+      "tools": ["React"]
+    },
+    {
+      "id": 3,
+      "company": "Account",
+      "logo": "../assets/account.svg",
+      "new": true,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2d ago",
+      "contract": "Part Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    },
+    {
+      "id": 4,
+      "company": "MyHome",
+      "logo": "../assets/myhome.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "5d ago",
+      "contract": "Contract",
+      "location": "USA Only",
+      "languages": ["CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 5,
+      "company": "Loop Studios",
+      "logo": "../assets/loop-studios.svg",
+      "new": false,
+      "featured": false,
+      "position": "Software Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["Ruby", "Sass"]
+    },
+    {
+      "id": 6,
+      "company": "FaceIt",
+      "logo": "../assets/faceit.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Backend Developer",
+      "role": "Backend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "UK Only",
+      "languages": ["Ruby"],
+      "tools": ["RoR"]
+    },
+    {
+      "id": 7,
+      "company": "Shortly",
+      "logo": "../assets/shortly.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["HTML", "JavaScript"],
+      "tools": ["Sass"]
+    },
+    {
+      "id": 8,
+      "company": "Insure",
+      "logo": "../assets/insure.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["Vue", "Sass"]
+    },
+    {
+      "id": 9,
+      "company": "Eyecam Co.",
+      "logo": "../assets/eyecam-co.svg",
+      "new": false,
+      "featured": false,
+      "position": "Full Stack Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "3w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript", "Python"],
+      "tools": ["Django"]
+    },
+    {
+      "id": 10,
+      "company": "The Air Filter Company",
+      "logo": "../assets/the-air-filter-company.svg",
+      "new": false,
+      "featured": false,
+      "position": "Front-end Dev",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "1mo ago",
+      "contract": "Part Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    }
+  ]
+
+  currentData = this.datas
+  filteredData: Array<{id: number, company: string, logo: string, new: boolean, featured: boolean, position: string, role: string, level: string, postedAt: string, contract: string, location: string, languages: string[], tools: string[] }> = []
+
+  filters: string[] = []
+
+  ngOnInit() {
+    this.createDataArray()
+  }
+
+  createDataArray(filter: string = '') {
+    console.log('Hello')
+    if(this.filters.length === 0) {
+      console.log('Hello no filter')
+      this.filteredData = this.datas
+    } else {
+      this.filteredData = []
+      console.log('Hello filtered', this.currentData, this.currentData.length, this.filteredData)
+      for(let i = 0; i < this.currentData.length; i++) {
+        console.log(this.currentData[i]['languages'].includes(filter) || this.currentData[i]['tools'].includes(filter))
+        if(this.currentData[i]['languages'].includes(filter) || this.currentData[i]['tools'].includes(filter)) {
+          console.log('Hello filtered ', this.currentData[i])
+          this.filteredData.push(this.datas[i])
+          console.log('Inside if ', this.filteredData)
+        }
+      }
+      console.log('Current data ', this.currentData)
+      this.currentData = this.filteredData
+    }
+  }
+
+  addFilter(value: string) {
+    if(!this.filters.includes(value)) { 
+      this.filters.push(value) 
+      this.createDataArray(value)
+    }
+  }
+
+  clearFilter() {
+    this.filters = []
+    this.currentData = this.datas
+    this.filteredData = this.datas
+    this.createDataArray()
+  }
+}
